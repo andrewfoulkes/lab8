ef55a84a7c5d00646827f23d733e4b6aeb24cf61
Andrew Crawford Foulkes <afoulkes@ucsd.edu>
2021-02-25 10:50:03 -0800

    lab8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,11 @@
 function initMap() {
-	// add your code here
+L.mapquest.key = 'EJPzNcvqqvMA1f0UGG6nlxrgMR3S3DAj';
+
+// 'map' refers to a <div> element with the ID map
+var map = L.mapquest.map('map', {
+  center: [32.87868246660001, -117.2358645444004],
+  layers: L.mapquest.tileLayer('map'),
+  zoom: 12
+});
+L.marker([32.87868246660001, -117.2358645444004]).addTo(map);
 }
